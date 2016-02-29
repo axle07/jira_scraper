b6dd98abcf91cb5fedea49ae657e7aeb207ff698
Alex <meetalexjohnson@gmail.com>
2016-02-29 14:40:15 -0700

    Initial commit

---
+++ scraper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COUNTER=0
+
+until [ $COUNTER -gt 2000 ]; do
+    #echo The counter is $COUNTER
+    let COUNTER=COUNTER+1
+
+    curl -b ../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' > SF-$COUNTER
+done
+
