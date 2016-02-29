448448074cf801008b75d4aaa014b6fabec6697d
Alex <meetalexjohnson@gmail.com>
2016-02-29 14:47:22 -0700

    Scraper V1

--- scraper.sh
+++ scraper.sh
@@ -6,6 +6,6 @@ until [ $COUNTER -gt 2000 ]; do
     #echo The counter is $COUNTER
     let COUNTER=COUNTER+1
 
-    curl -b ../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' > SF-$COUNTER
+    curl -b ../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' > ./output/SF-$COUNTER
 done
 
