d06d9df0673275021d9a7eed5847ab058fe52758
Alex <meetalexjohnson@gmail.com>
2016-02-29 15:31:56 -0700

    V2, now saves file to dir named after the issue

--- scraper.sh
+++ scraper.sh
@@ -6,6 +6,10 @@ until [ $COUNTER -gt 2000 ]; do
     #echo The counter is $COUNTER
     let COUNTER=COUNTER+1
 
-    curl -b ../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' > ./output/SF-$COUNTER
-done
+    mkdir ./attachments/SF-$COUNTER; cd ./attachments/SF-$COUNTER
+    curl -b ../../../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' | xargs curl -O -b ../cooky 
+    cd ../..
 
+
+    #curl -b ../../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' | xargs curl -O -b ../cooky
+done
