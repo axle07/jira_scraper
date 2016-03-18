35c405b9f31f9dbf493a3d392e3efb9273e443d4
Alex <meetalexjohnson@gmail.com>
2016-03-18 10:57:18 -0700

    Pipe across multiple lines

--- scraper.sh
+++ scraper.sh
@@ -10,6 +10,9 @@ COUNTER=0
 until [ $COUNTER -gt 2000 ]; do
     let COUNTER=COUNTER+1
     mkdir ./attachments/SF-$COUNTER; cd ./attachments/SF-$COUNTER
-    curl -b ../../../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER | egrep -o 'secure/attachment/[0-9]+/[^\"]+' | perl -ne 'print "https://spyfoo.atlassian.net/$_"' | xargs curl -O -b ../../../cooky 
+    curl -b ../../../cooky https://spyfoo.atlassian.net/browse/SF-$COUNTER |
+    egrep -o 'secure/attachment/[0-9]+/[^\"]+' |
+    perl -ne 'print "https://spyfoo.atlassian.net/$_"' |
+    xargs curl -O -b ../../../cooky 
     cd ../..
 done
